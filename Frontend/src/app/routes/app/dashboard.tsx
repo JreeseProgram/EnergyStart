cd7a96610d46f3067a5594f7640017b3766483c6
JreeseProgram <jreese17@mail.valenciacollege.edu>
2026-09-16 13:53:01 -0400

    fixed file structure
    File structures updated

---
+++ Frontend/src/app/routes/app/dashboard.tsx
@@ -0,0 +1,11 @@
+import { ContentLayout } from "@/components/layouts/content-layout"
+
+const DashboardRoute = () => {
+  return (
+    <ContentLayout title="Dashboard">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6"></div>
+    </ContentLayout>
+  )
+}
+
+export default DashboardRoute
---
+++ Frontend/src/hooks/use-login.ts
@@ -0,0 +1,40 @@
+import * as React from "react"
+import {
+  useMutation,
+  useQueryClient,
+  type UseMutationOptions,
+} from "@tanstack/react-query"
+import { Services } from "@/context/services.context"
+import type { AuthenticatedUser, User } from "@/models/user.models"
+import type { LoginInput } from "@/services/auth.service"
+
+type LoginParams = { input: LoginInput }
+
+export const login = ({
+  authService,
+  input,
+}: Pick<Services, "authService"> & LoginParams) => {
+  return authService.login(input)
+}
+
+export const useLogin = (
+  options?: UseMutationOptions<AuthenticatedUser, Error, LoginInput>
+) => {
+  const { authService } = React.use(Services)
+
+  const queryClient = useQueryClient()
+
+  const setUser = React.useCallback(
+    (data: User | null) => queryClient.setQueryData(["User"], data),
+    [queryClient]
+  )
+
+  return useMutation({
+    ...options,
+    mutationFn: (input) => login({ authService, input }),
+    onSuccess: (data, ...args) => {
+      setUser(data.user)
+      options?.onSuccess?.(data, ...args)
+    },
+  })
+}
